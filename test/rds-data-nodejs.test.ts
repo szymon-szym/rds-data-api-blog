f1d30bdae48afbd20b5a32e49866975bfad9c519
szymon-szym <szymon.mich@gmail.com>
2024-10-27 14:21:07 +0100

    Initial commit

---
+++ test/rds-data-nodejs.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as RdsDataNodejs from '../lib/rds-data-nodejs-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/rds-data-nodejs-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new RdsDataNodejs.RdsDataNodejsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
